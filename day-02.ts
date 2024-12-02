0657c5622446c03cbde9b4d52b2c7adcb504a468
Tanzim Mokammel <mtanzim@gmail.com>
2024-12-02 18:24:42 -0500

    fp

--- day-02.ts
+++ day-02.ts
@@ -7,6 +7,47 @@ const testData = `
 1 3 6 7 9
 `;
 
+(function main() {
+  console.log(ptA(testData));
+  console.log(ptB(testData));
+  Deno.readTextFile("./day-02.txt").then((r) => {
+    const resA = ptA(r);
+    console.log(resA);
+    const resB = ptB(r);
+    console.log(resB);
+  });
+})();
+
+function ptA(inp: string) {
+  return processIn(inp)
+    .map((line) => {
+      return isSafe(line) ? 1 : 0;
+    })
+    .reduce((acc, cur) => acc + cur, 0 as number);
+}
+
+function ptB(inp: string) {
+  return processIn(inp)
+    .map((line) => {
+      const isFullySafe = isSafe(line);
+      if (isFullySafe) {
+        return 1;
+      }
+      const subArrs = line.map((_, idx) =>
+        line.slice(0, idx).concat(line.slice(idx + 1, line.length))
+      );
+      return subArrs.some(isSafe) ? 1 : 0;
+    })
+    .reduce((acc, cur) => acc + cur, 0 as number);
+}
+
+function processIn(inp: string): number[][] {
+  return inp
+    .split("\n")
+    .filter(Boolean)
+    .map((l) => l.split(" ").map(Number));
+}
+
 function isSafe(line: number[]): boolean {
   let isSafe = true;
   let dir: "incr" | "decr" = "decr";
@@ -33,39 +74,3 @@ function isSafe(line: number[]): boolean {
   }
   return isSafe;
 }
-
-function ptA(inp: string) {
-  const lines = inp.split("\n").filter(Boolean);
-  const d = lines.map((l) => l.split(" ").map(Number));
-  const res: number[] = d.map((line, idx) => {
-    return isSafe(line) ? 1 : 0;
-  });
-  return res.reduce((acc, cur) => acc + cur, 0);
-}
-
-function ptB(inp: string) {
-  const lines = inp.split("\n").filter(Boolean);
-  const d = lines.map((l) => l.split(" ").map(Number));
-  const res: number[] = d.map((line, idx) => {
-    const isFullySafe = isSafe(line);
-    if (isFullySafe) {
-      return 1;
-    }
-    const subArrs = line.map((_, idx) =>
-      line.slice(0, idx).concat(line.slice(idx + 1, line.length))
-    );
-    return subArrs.some(isSafe) ? 1 : 0;
-  });
-  return res.reduce((acc, cur) => acc + cur, 0);
-}
-
-(function main() {
-  console.log(ptA(testData));
-  console.log(ptB(testData));
-  Deno.readTextFile("./day-02.txt").then((r) => {
-    const resA = ptA(r);
-    console.log(resA);
-    const resB = ptB(r);
-    console.log(resB);
-  });
-})();
